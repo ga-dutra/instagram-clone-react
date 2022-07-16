68eda2fd0f4c02f2f50461f04c3e68b7f34d891b
gacdutra <ga.acdutra@gmail.com>
2022-07-16 14:56:18 -0300

    componetização finalizada

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,58 @@
-import LogoDesktop from "./LogoDesktop";
-import IconesDesktop from "./IconesDesktop";
-import LogoMobile from "./LogoMobile";
-import IconesMobile from "./IconesMobile";
-
+function LogoDesktop() {
+  return (
+    <div class="logo">
+      <ion-icon name="logo-instagram"></ion-icon>
+      <div class="separador"></div>
+      <img src="assets/img/logo.png" />
+    </div>
+  );
+}
+function IconeLogoMobile() {
+  return (
+    <div class="logo-mobile">
+      <ion-icon name="logo-instagram"></ion-icon>
+    </div>
+  );
+}
+function LogoMobile() {
+  return (
+    <div class="instagram-mobile">
+      <img src="assets/img/logo.png" />
+    </div>
+  );
+}
+function BarraPesquisa() {
+  return (
+    <div class="pesquisa">
+      <input type="text" placeholder="Pesquisar" />
+    </div>
+  );
+}
+function IconesMobile() {
+  return (
+    <div class="icones-mobile">
+      <ion-icon name="paper-plane-outline"></ion-icon>
+    </div>
+  );
+}
+function IconesDesktop() {
+  return (
+    <div class="icones">
+      <ion-icon name="paper-plane-outline"></ion-icon>
+      <ion-icon name="compass-outline"></ion-icon>
+      <ion-icon name="heart-outline"></ion-icon>
+      <ion-icon name="person-outline"></ion-icon>
+    </div>
+  );
+}
 export default function Navbar() {
   return (
     <div class="navbar">
       <div class="container">
         <LogoDesktop />
+        <IconeLogoMobile />
         <LogoMobile />
-
-        <div class="instagram-mobile">
-          <img src="assets/img/logo.png" />
-        </div>
-
-        <div class="pesquisa">
-          <input type="text" placeholder="Pesquisar" />
-        </div>
-
+        <BarraPesquisa />
         <IconesDesktop />
         <IconesMobile />
       </div>
--- src/components/Sugestoes.js
+++ src/components/Sugestoes.js
@@ -1,4 +1,47 @@
+function ConteudoSugestoes(props) {
+  return (
+    <div class="sugestao">
+      <div class="usuario">
+        <img src={props.img} />
+        <div class="texto">
+          <div class="nome">{props.username}</div>
+          <div class="razao">{props.doesfollow}</div>
+        </div>
+      </div>
+      <div class="seguir">Seguir</div>
+    </div>
+  );
+}
+
 export default function Sugestoes() {
+  const sugestoes = [
+    {
+      img: "assets/img/bad.vibes.memes.svg",
+      username: "bad.vibes.memes",
+      doesfollow: "Segue você",
+    },
+    {
+      img: "assets/img/chibirdart.svg",
+      username: "chibirdart",
+      doesfollow: "Segue você",
+    },
+    {
+      img: "assets/img/razoesparaacreditar.svg",
+      username: "razoesparaacreditar",
+      doesfollow: "Novo no Instagram",
+    },
+    {
+      img: "assets/img/adorable_animals.svg",
+      username: "adorable_animals",
+      doesfollow: "Segue você",
+    },
+    {
+      img: "assets/img/smallcutecats.svg",
+      username: "smallcutecats",
+      doesfollow: "Segue você",
+    },
+  ];
+
   return (
     <div class="sugestoes">
       <div class="titulo">
@@ -6,65 +49,13 @@ export default function Sugestoes() {
         <div>Ver tudo</div>
       </div>
 
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="assets/img/bad.vibes.memes.svg" />
-          <div class="texto">
-            <div class="nome">bad.vibes.memes</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="assets/img/chibirdart.svg" />
-          <div class="texto">
-            <div class="nome">chibirdart</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="assets/img/razoesparaacreditar.svg" />
-          <div class="texto">
-            <div class="nome">razoesparaacreditar</div>
-            <div class="razao">Novo no Instagram</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="assets/img/adorable_animals.svg" />
-          <div class="texto">
-            <div class="nome">adorable_animals</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="assets/img/smallcutecats.svg" />
-          <div class="texto">
-            <div class="nome">smallcutecats</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
+      {sugestoes.map((items) => (
+        <ConteudoSugestoes
+          img={items.img}
+          username={items.username}
+          doesfollow={items.doesfollow}
+        />
+      ))}
     </div>
   );
 }
